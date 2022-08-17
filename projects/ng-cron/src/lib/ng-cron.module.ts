7e4bee7427d40c35b79b82471d6d2a3d2a1e6166
SERHIIBZENKD8F4\serhiibzenko <bzenkosergey@gmail.com>
2022-08-17 20:46:07 +0200

    initial

---
+++ projects/ng-cron/src/lib/ng-cron.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { NgCronComponent } from './ng-cron.component';
+
+
+
+@NgModule({
+  declarations: [
+    NgCronComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    NgCronComponent
+  ]
+})
+export class NgCronModule { }
---
+++ projects/ng-cron/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of ng-cron
+ */
+
+export * from './lib/ng-cron.service';
+export * from './lib/ng-cron.component';
+export * from './lib/ng-cron.module';
